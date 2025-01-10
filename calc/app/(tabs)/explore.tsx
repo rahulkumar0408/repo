5927f3b7eb834df02ca00443723bf654c9152a94
rahulkumar0408 <rahulsewaliya1245@gmail.com>
2025-01-11 00:23:53 +0530

    flatelist

--- calc/app/(tabs)/explore.tsx
+++ calc/app/(tabs)/explore.tsx
@@ -5,12 +5,11 @@ import cardData from "@/interface/cardData";
 import { ScrollView } from "react-native-gesture-handler";
 let data=cardData;
 export default function ExploreApp() {
-  const image =
-    "https://books.google.co.in/books/publisher/content?id=_ZOvEAAAQBAJ&pg=PA1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U07Nf7ty1PqB-A6wPoezpNnQj-27w&w=1280";
+
   return (
     <>
     <ScrollView >
-
+{/* 
     {
       
       data.map((item)=>( 
@@ -23,7 +22,28 @@ export default function ExploreApp() {
         url={item.url}
         />
       ) 
-    )    }
+    )    } */}
+
+
+{/*     
+    <FlatList 
+    data={cardData}
+    renderItem={({item}) => (
+      <Card
+      title={item.title}
+      productName={item.productName}
+      
+      oldPrice={item.oldPrice}
+      finalPrice={item.finalPrice}
+      url={item.url}
+      />
+    )}
+    numColumns={4}
+
+    /> */}
+
+
+    
     </ScrollView>
     </>
   );
